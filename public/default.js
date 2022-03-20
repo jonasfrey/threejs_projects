915fc26337634903836b496682f064f555699714
jf18j492 <jonas.frey@csh.unibe.ch>
2022-03-21 00:18:27 +0100

    added plane and light

--- public/default.js
+++ public/default.js
@@ -29,6 +29,30 @@ var o_orbit_controls = new OrbitControls( o_camera, document.body );
 o_orbit_controls.target.set( 0, 1.6, 0 );
 o_orbit_controls.update();
 
+
+const floorGeometry = new THREE.PlaneGeometry( 4, 4 );
+const floorMaterial = new THREE.MeshStandardMaterial( {
+    color: 0xeeeeee,
+    roughness: 1.0,
+    metalness: 0.0
+} );
+const floor = new THREE.Mesh( floorGeometry, floorMaterial );
+floor.rotation.x = - Math.PI / 2;
+floor.receiveShadow = true;
+o_scene.add( floor );
+
+o_scene.add( new THREE.HemisphereLight( 0x808080, 0x606060 ) );
+
+const light = new THREE.DirectionalLight( 0xffffff );
+light.position.set( 0, 6, 0 );
+light.castShadow = true;
+light.shadow.camera.top = 2;
+light.shadow.camera.bottom = - 2;
+light.shadow.camera.right = 2;
+light.shadow.camera.left = - 2;
+light.shadow.mapSize.set( 4096, 4096 );
+o_scene.add( light );
+
 var n_camera_movement_speed = 0.02;
 var o_renderer = new THREE.WebGLRenderer();
 o_renderer.setSize(window.innerWidth, window.innerHeight);
