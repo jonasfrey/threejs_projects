e38f3296960c96b062b97066b0cadb16a394e056
jf18j492 <jonas.frey@csh.unibe.ch>
2022-03-24 00:29:40 +0100

    todo: multiple properties linking

--- public/waswowie2.js
+++ public/waswowie2.js
@@ -74,7 +74,7 @@ var f_link_properties = function(
 
 // layout object gets a reference to a threejs object
 // layout object get9 => { t: PointLight_instance_reference, ":intensity": "intensity_dark" }
-
+window.THREE = THREE
 
 
 window.scene = new THREE.Scene();
@@ -90,32 +90,109 @@ window.o_data = {
     "intensity_dark": 0.5, 
     "intensity_bright": 2,
     position_z : 3,
+    position_y: 0, 
 };
 
-window.o_layout = {
-    "t": PointLight, 
-    "<>intensity": "intensity_dark",
-    "position": {
-        x: 1,
-        y: 1,
-        "<>z": "position_z",
-    }
-}
+// window.o_layout = {
+//     "t": PointLight, 
+//     "<>intensity": "intensity_dark",
+//     "position": {
+//         x: 1,
+//         y: 1,
+//         "<>z": "position_z",
+//     }
+// }
 
+
+window.o_layout = {
+    t: Group, 
+    c:[
+        {
+            "t": PointLight, 
+            "<>intensity": "intensity_dark",
+            "position": {
+                x: 1,
+                y: 1,
+                "<>z": "position_z",
+            }
+        }, 
+        {
+            "t": Mesh, 
+            "geometry": {
+                    "t": BoxGeometry, 
+                }, 
+            "material": {
+                "t": MeshPhongMaterial, 
+                "color": {
+                    r: 222, 
+                    g:0, 
+                    b:0
+                }
+            },
+            scale: {
+                x: 0.5,
+                y: 0.5, 
+                z: 0.5
+            },
+            position: {
+                x: 1, 
+                 "<>y": "position_y", 
+                // y: 1,
+                z: 1
+            }
+        },
+        {
+            "t": Mesh, 
+            "geometry": {
+                    "t": BoxGeometry, 
+                }, 
+            "material": {
+                "t": MeshPhongMaterial, 
+                "color": 0xff00ff
+            },
+            scale: {
+                x: 0.5,
+                y: 0.5, 
+                z: 0.5
+            },
+            position: {
+                x: 1, 
+                 "<>y": "position_y", 
+                // y: 1,
+                z: 1
+            }
+        }
+    ]
+}
 var f_render_o_layout = function(object, object_parent){
     var o_for_linking = object_parent
+
     if(object.hasOwnProperty("t")){
         var o_t = new object.t()
         window.o_t = o_t
         o_for_linking = o_t
-        scene.add(o_t)
+        object.o_t = o_t
     }
 
     for(var s_prop in object){
         var value = object[s_prop]
-        // handle recursive objects 
-        if(typeof value === "object"){
-            value = f_render_o_layout(value, o_t[s_prop])
+        // handle recursive objects
+        if(
+            typeof value === "object" && 
+            Array.isArray(value) == false && 
+            // value.hasOwnProperty("t") == false && 
+            ["t", "c", "o_t"].indexOf(s_prop) == -1
+        ){
+            if(value.hasOwnProperty("t") == false){
+                // console.log(o_t[s_prop])
+                // console.log("o_t[s_prop]")
+                value = f_render_o_layout(value, o_t[s_prop])
+            }else{
+                // console.log(object)
+                // console.log("object")
+                o_t[s_prop] = f_render_o_layout(value, object)
+
+            }
         }
         // handle linking 
         if(s_prop.indexOf('<>') == 0){ //<> sync in both directions
@@ -132,38 +209,39 @@ var f_render_o_layout = function(object, object_parent){
         if(
             s_prop.indexOf('<>') != 0 &&
             typeof value !== "object" && 
-            ["t", "c"].indexOf(s_prop) == -1 // "t" and "c" are keyword properties, todo: check if threejs uses them somewhere
+            ["t", "c", "o_t"].indexOf(s_prop) == -1 // "t" and "c" are keyword properties, todo: check if threejs uses them somewhere
         ){
-            console.log(s_prop)
-            object_parent[s_prop] = value
+            try {
+                object_parent[s_prop] = value
+                
+            } catch (error) {
+                debugger
+            }
         }
 
         //handle children objects 
         if(s_prop == "c"){
             for(var s_c_index in object.c){
                 var o_c_object = object.c[s_c_index]
-                var o_handled_c_object = f_render_o_layout(value, object)
-                o_t.add(o_handled_c_objects)
+                var o_handled_c_object = f_render_o_layout(o_c_object, object)
+                
+                object.o_t.add(o_handled_c_object)
             }
         }
     }
-    return object
+    if(o_t){
+        return o_t
+    }else{
+        return object
+    }
 }
 o_layout = f_render_o_layout(o_layout) 
-
+scene.add(o_layout)
 // const geometry = new THREE.BoxGeometry();
 // const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
 // const cube = new THREE.Mesh( geometry, material );
 // // cube.position.copy(new THREE.Vector3(1,2,3))
 // scene.add( cube );
-
-
-
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-// const material = new THREE.MeshPhongMaterial( {color: 0x00ff00} );
-const material = new THREE.MeshPhongMaterial( {color: 0xffffff} );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
 
 
 // const light = new THREE.PointLight( 0xffffff, 1, 100 );
